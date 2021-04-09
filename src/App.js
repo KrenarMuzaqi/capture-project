036b0c362ddb2f58386dc1649163be3e5509a9de
Krenar <krenarmuzaqi@gmail.com>
2021-04-10 00:04:25 +0200

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 //Import Sections
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
